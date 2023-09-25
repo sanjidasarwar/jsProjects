23784ed337063007b7661f668d24ffced303517a
tupsi07 <sanjida657@yahoo.com>
2023-09-25 12:10:31 +0600

    12 hours format setting

--- DigitalClock/script.js
+++ DigitalClock/script.js
@@ -1,10 +1,25 @@
 function createClock(){
     let date = new Date();
-    const hours=date.getHours()
-    const min =date.getMinutes()
-    const sec = date.getSeconds()
+    let hr=date.getHours()
+    let min =date.getMinutes()
+    let sec = date.getSeconds()
+    let am_pm = 'AM'
+
+    // Setting time for 12 Hrs format
+   if(hr>12){
+        hr -= 12
+        am_pm = 'PM'
+   }
+   if(hr==0){
+        hr = 12
+   }
+
+   hr = (hr<10 )? '0' + hr : hr
+   min = (min<10 )? '0' + min : min
+   sec = (sec<10 )? '0' + sec : sec
+
     let clock= document.getElementById('clockDiv');
-    clock.innerText= hours+':'+ min + ":" + sec
+    clock.innerText= hr+':'+ min + ":" + sec + am_pm
 }
 
 setInterval(createClock, 1000)
