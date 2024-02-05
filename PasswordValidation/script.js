f8a954609e0ff7b83944a39790755a6d5cb55000
tupsi07 <sanjida657@yahoo.com>
2024-02-06 00:53:25 +0600

    confirm password functional

--- PasswordValidation/script.js
+++ PasswordValidation/script.js
@@ -8,6 +8,14 @@ const password={
             input.type = 'password'
             e.target.classList.replace('fa-eye-slash','fa-eye');
         }
+    },
+    confirmPassword: function () {
+        const password = document.getElementById('password').value
+        const confimPassword = document.getElementById('confimPassword').value
+
+        if(confimPassword.length && confimPassword !==password){
+            alert('Password and Confirm Password not matched')
+        }
     }
 }
 
@@ -17,3 +25,10 @@ const showBtn = document.querySelectorAll('.show')
 showBtn.forEach(btn=>{
     btn.addEventListener('click', password.showAndHidePassword)
 })
+
+const form = document.getElementById('passwordForm')
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    password.confirmPassword()
+})
+
