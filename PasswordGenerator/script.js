8e1d2bd9ca4cb4f26660da2f2a8a098a4b9289fc
tupsi07 <sanjida657@yahoo.com>
2024-03-15 00:22:56 +0600

    password generator- generate button activate

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,44 @@
+const inputText = document.querySelector('#inputText')
+const copyBtn = document.querySelector('#copy')
+const length = document.querySelector('#length')
+const uppercase = document.querySelector('#uppercase')
+const lowercase = document.querySelector('#lowercase')
+const number = document.querySelector('#number')
+const symbol = document.querySelector('#symbol')
+const generateBtn = document.querySelector('#generateBtn')
+
+
+const uppercaseValue = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const lowercaseValue = 'abcdefghijklmnopqrstuvwxyz'
+const numberValue = '0123456789'
+const symbolValue = '!@#$%^&*()?.<\>|=+:;,[-_]'
+
+
+generateBtn.addEventListener('click', function(){
+    let selectedValue= ''
+
+    if(uppercase.checked){
+        selectedValue += uppercaseValue
+    }
+    if(lowercase.checked){
+        selectedValue += lowercaseValue
+    }
+    if(number.checked){
+        selectedValue += numberValue
+    }
+    if(symbol.checked){
+        selectedValue += symbolValue
+    }
+
+    let password ='';
+
+    for(let i=0; i<length.value; i++ ){
+        let index = Math.floor(Math.random(selectedValue) * selectedValue.length)
+        password +=selectedValue[index]
+    }
+
+    inputText.value = password
+
+
+})
+
