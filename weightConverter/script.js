8a977655e3180e95727eb3f48cc67e5801d4923d
tupsi07 <sanjida657@yahoo.com>
2024-03-20 00:00:39 +0600

    WeightConverter- simplified the code

--- weightConverter/script.js
+++ weightConverter/script.js
@@ -1,63 +1,52 @@
-const initailWeight = document.querySelector('#initailWeight')
-const initailWeightValue = document.querySelector('#initailWeightValue')
-const convertedWeight = document.querySelector('#convertedWeight')
-let convertedWeightValue = document.querySelector('#convertedWeightValue')
-const weightOptions = document.querySelectorAll('.weightOptions')
+const initailWeight = document.querySelector("#initailWeight");
+const initailWeightInput = document.querySelector("#initailWeightValue");
+const convertedWeight = document.querySelector("#convertedWeight");
+let convertedWeightInput = document.querySelector("#convertedWeightValue");
+const weightOptions = document.querySelectorAll(".weightOptions");
 
-weightOptions.forEach(select=>{
-    select.addEventListener('change', function(){
-        const allOptions = this.options
-    
-        for(let i=0; i<allOptions.length;i++){
-            if(allOptions[i].hasAttribute('selected')){
-                allOptions[i].removeAttribute('selected')
-            }
-        }
-    
-        allOptions[this.selectedIndex].setAttribute('selected', 'selected')
-    
-    })
-    
-})
+initailWeightInput.value = '1';
 
-function calculateWeight (inputField){
-    let input;
-    let result;
+weightOptions.forEach((select) => {
+  select.addEventListener("change", function () {
+    const allOptions = this.options;
 
-    if(inputField.id == 'convertedWeightValue'){
-        input = convertedWeightValue.value;
-    }else{
-        input = initailWeightValue.value;
+    for (let i = 0; i < allOptions.length; i++) {
+      if (allOptions[i].hasAttribute("selected")) {
+        allOptions[i].removeAttribute("selected");
+      }
     }
 
-    if(initailWeight.value=='kg' && convertedWeight.value=='pound'){
-        if(inputField.id == 'convertedWeightValue'){
-            result = input /2.20462
+    allOptions[this.selectedIndex].setAttribute("selected", "selected");
+  });
+});
 
-            initailWeightValue.value = result.toFixed(4);
-            // to activate the field editable
-            initailWeightValue.removeAttribute('readonly');
-        }else{
-            result = input *2.20462
+function calculateWeight(inputField) {
+  let input;
+  let result;
+  console.log(inputField.id);
+  if (inputField.id == "initailWeightValue") {
+    input = initailWeightInput.value;
+  } else if (inputField.id == "convertedWeightValue") {
+    input = convertedWeightInput.value;
+  }
 
-            convertedWeightValue.value = result.toFixed(4);
-            // to activate the field editable
-            convertedWeightValue.removeAttribute('readonly');
-        }    
-    }                      
+  if (initailWeight.value == "kg" && convertedWeight.value == "pound") {
+    result = inputField.id == "initailWeightValue" ? input * 2.20462 : input / 2.20462;
+  } else if (initailWeight.value == "pound" && convertedWeight.value == "kg") {
+    result = inputField.id == "initailWeightValue" ? input / 2.20462 : input * 2.20462;
+  }
 
-
+  const outputField = inputField.id == "initailWeightValue" ? convertedWeightInput : initailWeightInput
+  
+  outputField.value = result.toFixed(4);
+  outputField.removeAttribute('readonly');
 }
 
-initailWeightValue.addEventListener('input', function(){
-    calculateWeight(initailWeightValue)
-})
-convertedWeightValue.addEventListener('input', function(){
-    calculateWeight(convertedWeightValue)
-})
+initailWeightInput.addEventListener("input", function () {
+  calculateWeight(initailWeightInput);
+});
+convertedWeightInput.addEventListener("input", function () {
+  calculateWeight(convertedWeightInput);
+});
 
-
-
-calculateWeight(initailWeightValue)
-
-
+calculateWeight(initailWeightInput);
